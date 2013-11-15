e49294a3d482bf3b35fd50ca80bd11ded5c20df4
Mihail Yakimenko <macgera@gmail.com>
2013-11-15 20:51:27 +0200

    css for main blocks

--- js/func.js
+++ js/func.js
@@ -1,7 +1,7 @@
 var Gallery = function(){
     $('.gallery_images').cycle({ 
         fx:     'fade',
-        speed:  'fast',
+        speed:  'normal',
         timeout: 0, 
         next:   '.control_left', 
         prev:   '.control_right'
